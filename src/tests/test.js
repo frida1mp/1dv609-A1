4bf190ccc49d16bfd97b0a654358aba9511c63bc
Frida Pedersén <fp222ni@student.lnu.se>
2024-11-21 11:07:46 +0200

    REFACTOR: remove repition of instanciating account class

--- src/tests/test.js
+++ src/tests/test.js
@@ -11,25 +11,26 @@ jest.mock("./src/accountManager.js", () => {
     })
   }
 })
+describe('BookingManager', () => {
+  let account
 
-test('should create an account with an initial balance of 0', () => {
-  const account = new Account()
-  expect(account.getBalance()).toBe(0)
+  beforeEach(() => {
+    const mockAccountManager = new AccountManager()
+    account = new Account(mockAccountManager)
+  })
+
+
+  test('should create an account with an initial balance of 0', () => {
+    expect(account.getBalance()).toBe(0)
+  })
+
+  test('should deposit money into the current balance', () => {
+    account.deposit(50)
+    expect(account.getBalance()).toBe(50)
+  })
+
+  test('should log transaction message after depositing money', () => {
+    account.deposit(40)
+    expect(account.deposit(40)).toBe(`Deposited ${40}`)
+  })
 })
-
-test('should deposit money into the current balance', () => {
-  const mockAccountManager = new AccountManager()
-
-  const account = new Account(mockAccountManager)
-  account.deposit(50)
-  expect(account.getBalance()).toBe(50)
-})
-
-test('should log transaction message after depositing money', () => {
-  const mockAccountManager = new AccountManager()
-  const account = new Account(mockAccountManager)
-
-  account.deposit(40)
-  expect(account.deposit(40)).toBe(`Deposited ${40}`)
-})
-
