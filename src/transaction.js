49200374620eb0b76c1c800a29480b3243920cd2
Frida Pedersén <fp222ni@student.lnu.se>
2024-11-28 09:25:02 +0200

    GREEN: throws error when number input is invalid, passed

--- src/transaction.js
+++ src/transaction.js
@@ -3,6 +3,10 @@ export class Transaction {
         if (typeof type != 'string') {
             throw new Error('Type of transaction needs to be deposit or withdraw')
         }
+
+        if (typeof amount != 'number') {
+            throw new Error('Type of transaction needs to be a number')
+        }
         this.type = type
         this.amount = amount
         this.time = new Date()
